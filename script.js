44bd5d10a6090ea45e1456f2afbd4efd2fc61ec8
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-06 21:07:55 +0200

    add countdown to month

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ function updateCountdown() {
     const progressBarMinutes = document.querySelector('.progress-minutes');
     const progressBarHours = document.querySelector('.progress-hours');
     const progressBarDay = document.querySelector('.progress-day');
+    const progressBarMonth = document.querySelector('.progress-month');
 
     const secondsUntilNextMinute = 60 - now.getSeconds();
     const minutesUntilNextHour = 60 - now.getMinutes();
     const hoursUntilNextDay = 24 - now.getHours();
 
-    // console.log(minutesUntilNextHour);
+    const daysInCurrentMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+    const daysUntilNextMonth = daysInCurrentMonth - now.getDate();
+    
+    
     
     progressBarMinutes.style.width = ((60-secondsUntilNextMinute) / 60) * 100 + '%';
     progressBarHours.style.width = ((60 - minutesUntilNextHour) / 60) * 100 + '%';
     progressBarDay.style.width = ((24 - hoursUntilNextDay) / 24) * 100 + '%';
+    progressBarMonth.style.width = (now.getDate() / daysInCurrentMonth) * 100 + '%';
     
     document.getElementById('countdown-minutes').textContent = `${secondsUntilNextMinute} seconds left`;
     document.getElementById('countdown-hours').textContent = `${minutesUntilNextHour} minutes left`;
     document.getElementById('countdown-day').textContent = `${hoursUntilNextDay} hours left`;
+    document.getElementById('countdown-month').textContent = `${daysUntilNextMonth} days left`;
 
 }
 
