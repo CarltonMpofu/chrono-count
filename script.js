ccbeddf55d8f44abc299bef134e29031f44d7da5
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-06 20:28:36 +0200

    Fix countdown timer

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function updateCountdown() {
     const now = new Date();
-    const secondsUntilNextMinute = 60 - now.getSeconds();
+    
     const progressBarMinutes = document.querySelector('.progress-minutes');
     const progressBarHours = document.querySelector('.progress-hours');
+    const progressBarDay = document.querySelector('.progress-day');
 
-    const secondsPassedToday = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+    const secondsUntilNextMinute = 60 - now.getSeconds();
+    const minutesUntilNextHour = 60 - now.getMinutes();
+    const hoursUntilNextDay = 24 - now.getHours();
+
+    // console.log(minutesUntilNextHour);
     
-    progressBarMinutes.style.width = ((60 - secondsUntilNextMinute) / 60) * 100 + '%';
-    progressBarHours.style.width = (now.getMinutes() * 60  / 3600) * 100 + '%';
-    // console.log(secondsPassedToday);
-    //progressBarHours.style.width = (secondsPassedToday / 86400) * 100 + '%';
-
-
-    document.getElementById('countdown').textContent = `00:${secondsUntilNextMinute < 10 ? '0' : ''}${secondsUntilNextMinute}`;
-
-    const secondsUntilNextHour = 3600 - secondsPassedToday % 3600;
-    const hours = Math.floor(secondsUntilNextHour / 3600);
-    const minutes = Math.floor((secondsUntilNextHour % 3600) / 60);
-    const seconds = secondsUntilNextHour % 60;
-    document.getElementById('countdown-hours').textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    progressBarMinutes.style.width = ((60-secondsUntilNextMinute) / 60) * 100 + '%';
+    progressBarHours.style.width = ((60 - minutesUntilNextHour) / 60) * 100 + '%';
+    progressBarDay.style.width = ((24 - hoursUntilNextDay) / 24) * 100 + '%';
+    
+    document.getElementById('countdown-minutes').textContent = `${secondsUntilNextMinute} seconds left`;
+    document.getElementById('countdown-hours').textContent = `${minutesUntilNextHour} minutes left`;
+    document.getElementById('countdown-day').textContent = `${hoursUntilNextDay} hours left`;
 
 }
 
