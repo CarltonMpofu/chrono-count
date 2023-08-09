eee5adea5b3155890f74a1ddda6ef76ab404171a
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-09 23:31:50 +0200

    Add backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res)
+{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/add-day", function(req, res)
+{
+    res.sendFile(__dirname + "/addDay.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
