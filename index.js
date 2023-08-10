5b28e5f5a5e8e98eefae9d0c9b6cc633e8509407
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-08-10 21:20:54 +0200

    Add mongoose and route for saving days to database

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.static("public"));
+
+mongoose.connect('mongodb://127.0.0.1:27017/birthdayDB');
+
+const birthdaySchema = new mongoose.Schema({title: String, date:String});
+
+const Birthday = mongoose.model("Birthday", birthdaySchema);
 
 app.get("/", function(req, res)
 {
@@ -15,10 +22,23 @@ app.get("/add-day", function(req, res)
     res.sendFile(__dirname + "/addDay.html");
 });
 
-app.post("/add-day", function(req, res)
+app.post("/add-day", async function(req, res)
 {
     const title = req.body.title;
     const date = req.body.date;
+
+    try
+    {
+        const newBirthday = new Birthday({title: title, date:date});
+        //await newBirthday.save();
+        res.redirect("/");
+    }
+    catch(error)
+    {
+        res.send(error);
+    }
+    
+
 });
 
 app.listen(3000, function(){
